2342d934a3ebb98692d108a426b670e4f8395836
josephrocca <1167575+josephrocca@users.noreply.github.com>
2022-03-10 21:42:59 +0800

    Fix onnx export error
    The error in question:
    ```
    /content/projected_gan/pg_modules/blocks.py:40: TracerWarning: Converting a tensor to a Python boolean might cause the trace to be incorrect. We can't record the data flow of Python values, so this value will be treated as a constant in the future. This means that the trace might not generalize to other inputs!
      assert nc % 2 == 0, 'channels dont divide 2!'
    ```

--- pg_modules/blocks.py
+++ pg_modules/blocks.py
@@ -37,7 +37,7 @@ def NormLayer(c, mode='batch'):
 class GLU(nn.Module):
     def forward(self, x):
         nc = x.size(1)
-        assert nc % 2 == 0, 'channels dont divide 2!'
+        # assert nc % 2 == 0, 'channels dont divide 2!'
         nc = int(nc/2)
         return x[:, :nc] * torch.sigmoid(x[:, nc:])
 
